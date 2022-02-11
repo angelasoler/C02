25f48e4679aed5bc9ad9be74122863c2e25ce635
asoler <asoler@student.42sp.org.br>
2022-02-11 12:20:37 +0100

    binary output is making nothing ex02

---
+++ ex02/ft_str_is_alpha.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/11 11:54:58 by asoler            #+#    #+#             */
+/*   Updated: 2022/02/11 12:19:22 by asoler           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_alpha(char *str)
+{
+	int	result;
+	int	i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+			result = 1;
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			result = 1;
+		else if (str[i] >= 'a' && str[i] <= 'z')
+			result = 1;
+		else
+			result = 0;
+	}
+	return (result);
+}
+
+int	main()
+{
+	int n;
+	char s[26] = "askfkljasfas215685432AVHHA";
+	char v[19]= "skfk:lj>85<4.AVHHA";
+
+	n = ft_str_is_alpha(s);
+	printf("%d\n", n);
+	n = ft_str_is_alpha(v);
+	printf("%d\n", n);
+}
