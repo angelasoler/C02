638cf79c0b6b0239071237a0654292e39ba5d89c
asoler <asoler@student.42sp.org.br>
2022-02-11 22:26:09 +0100

    wip no clue why is notprinting 1 + just alphabet and not numeric corrected

--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -6,7 +6,7 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 11:54:58 by asoler            #+#    #+#             */
-/*   Updated: 2022/02/11 16:13:57 by asoler           ###   ########.fr       */
+/*   Updated: 2022/02/11 22:23:05 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,14 @@ int	ft_str_is_alpha(char *str)
 	int	i;
 
 	i = 0;
-
 	while (str[i] != '\0')
 	{
-		if (str[i] >= '0' && str[i] <= '9')
-		{
-			i++;
-		}
 		if (str[i] >= 'A' && str[i] <= 'Z')
-		{
 			i++;
-		}
 		if (str[i] >= 'a' && str[i] <= 'z')
-		{
 			i++;
-		}
 		else
-		{
 			return (0);
-		}
 	}
 	return (1);
 }
@@ -45,9 +34,12 @@ int	main()
 	int n;
 	char s[27] = "askfkljasfas215685432AVHHA";
 	char v[19]= "skfk:lj>85<4.AVHHA";
+	char alpha[6] = "FkjvR";
 
 	n = ft_str_is_alpha(s);
 	printf("%d\n", n);
 	n = ft_str_is_alpha(v);
+	printf("%d\n", n);
+	n = ft_str_is_alpha(alpha);
 	printf("%d\n", n);
 }
