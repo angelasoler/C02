bfb1d262d397e08829566bd872aec822a3c16997
asoler <asoler@student.42sp.org.br>
2022-02-11 16:15:45 +0100

    wip is not recognizing my alfanumeric string, should return 1

--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -6,7 +6,7 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 11:54:58 by asoler            #+#    #+#             */
-/*   Updated: 2022/02/11 12:27:12 by asoler           ###   ########.fr       */
+/*   Updated: 2022/02/11 16:13:57 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,36 @@
 
 int	ft_str_is_alpha(char *str)
 {
-	int	result;
 	int	i;
 
 	i = 0;
-	result = 0;
 
 	while (str[i] != '\0')
 	{
-		if (str[i] >= '0' || str[i] <= '9')
-			result = 1;
-		if (str[i] >= 'A' || str[i] <= 'Z')
-			result = 1;
-		if (str[i] >= 'a' || str[i] <= 'z')
-			result = 1;
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			i++;
+		}
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			i++;
+		}
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			i++;
+		}
+		else
+		{
+			return (0);
+		}
 	}
-	return (result);
+	return (1);
 }
 
 int	main()
 {
 	int n;
-	char s[26] = "askfkljasfas215685432AVHHA";
+	char s[27] = "askfkljasfas215685432AVHHA";
 	char v[19]= "skfk:lj>85<4.AVHHA";
 
 	n = ft_str_is_alpha(s);
