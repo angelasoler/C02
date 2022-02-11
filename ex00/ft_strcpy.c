396fde0452fcf32b3dd584f2b0bfe168b72ad366
asoler <asoler@student.42sp.org.br>
2022-02-11 02:22:31 +0100

    segmantation fault again

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 19:45:28 by asoler            #+#    #+#             */
-/*   Updated: 2022/02/10 19:45:50 by asoler           ###   ########.fr       */
+/*   Updated: 2022/02/11 02:20:36 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,48 @@
 
 char	*ft_strcpy(char *dest, char *src)
 {
-	dest = src;
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
 	return(dest);
 }
+
+
+int count(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		i++;
+	}
+	return (i+1);
+}
+
 
 int	main(void)
 {
 	char *source;
-	source = "Olá, quero transapassar essa string para o seu destino";
 	char *destiny;
 	char *result;
+	int size;
+
+	source = "Olá, quero transpassar essa string para o seu destino";
 	destiny = NULL;
-	
+
 	result = ft_strcpy(destiny, source);
-	printf("%s", result);
+
+	size = count(result);
+
+	if (result[size] == '\0')
+	{
+		printf("%s", result);
+	}
 }
