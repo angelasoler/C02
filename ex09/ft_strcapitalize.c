1499975b3d13c54b387ed913ba01a5148ddfb87b
asoler <asoler@student.42sp.org.br>
2022-02-13 14:17:50 +0100

    if position before char is not alphanumerique this char becomes upcase

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
+int	ft_char_is_alphanumeric(char *str)
+{
+
+	if (*str >= 'A' && *str <= 'Z')
+		return (1);
+	else if (*str >= 'a' && *str <= 'z')
+		return (1);
+	else if (*str >= '0' && *str <= '9')
+		return (1);
+	else
+		return (0);
+}
+
 char	*ft_strcapitalize(char *str)
 {
 	int	i;
 
 	i = 0;
-	//verificar retorno, pois string original nao deve mudar
 	while (str[i] != '\0')
 	{
 		if (str[i] >= 65 && str[i] <= 90)
@@ -13,19 +25,10 @@ char	*ft_strcapitalize(char *str)
 			str[i] += 32;
 			i++;
 		}
-		if ((str[i] >= 97 && str[i] <= 122) || str[i] == 20)
+		if ((str[i] >= 97 && str[i] <= 122) && ft_char_is_alphanumeric(&str[i - 1]) == 0)
 		{
-			if (i == 0)
-			{
-				str[i] -= 32;
-				i++;
-			}
-			else if (str[i] == 20)
-			{
-				i++;
-				str[i] -= 32;
-				i++;
-			}
+			str[i] -= 32;
+			i++;
 		}
 		else
 		{
@@ -37,14 +40,19 @@ char	*ft_strcapitalize(char *str)
 
 int	main()
 {
-	char *n;
 	char s[27] = "hola esta td em minuscula";
 	char v[19]= "eU SoUOU anG3La";
+	char q[100] = "oi, tudo bem? 42palavras quarenta-e-duas; cinquenta+e+um";
+	char *result;
 
-	n = ft_strcapitalize(s);
-	printf("%s\n", n);
-	n = ft_strcapitalize(v);
-	printf("%s\n", n);
+	result = ft_strcapitalize(s);
+	printf("%s\n", result);
+
+	result = ft_strcapitalize(q);
+	printf("%s\n", result);
+	
+	result = ft_strcapitalize(v);
+	printf("%s\n", result);
 }
 
 
